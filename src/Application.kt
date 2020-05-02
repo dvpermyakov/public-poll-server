a0b71fb25d6a92afa7b1235e0b286496e39d05c9
dmitrii.permiakov <dmitrii.permiakov@revolut.com>
2020-05-02 15:56:50 +0300

    update classes

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,19 @@
 package com.public.poll
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.tomcat.Tomcat
 
-fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
-
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun main(args: Array<String>) {
+    val server = embeddedServer(Tomcat, port = 8080) {
+        routing {
+            get("/api/poll/feed") {
+                call.respondText("HELLO WORLD!\n")
+            }
+        }
+    }
+    server.start(wait = true)
 }
-
