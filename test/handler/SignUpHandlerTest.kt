a30598db80fe51de5c566fec3fdf952aa5df3cba
Dmitrii Permiakov <38990835+dvpermyakov@users.noreply.github.com>
2020-05-04 21:14:33 +0300

    Feature/write tests for auth handlers (#17)
    * add sign up handelr test
    
    * add sign in handelr test
    
    * update config.yml
    
    * test
    
    * revert
    
    * test 2
    
    * fix tests
    
    Co-authored-by: dmitrii.permiakov <dmitrii.permiakov@revolut.com>

---
+++ test/handler/SignUpHandlerTest.kt
@@ -0,0 +1,79 @@
+package handler
+
+import com.public.poll.dto.ErrorDto
+import com.public.poll.dto.TokenDto
+import com.public.poll.dto.UserDto
+import com.public.poll.handler.auth.SignUpHandler
+import com.public.poll.repositories.UserRepository
+import io.ktor.http.HttpStatusCode
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class SignUpHandlerTest {
+
+    private val userRepository = mockk<UserRepository>()
+
+    private val signUpHandler = SignUpHandler(
+        userRepository = userRepository
+    )
+
+    @Test
+    fun `empty token`() {
+        val tokenDto = TokenDto("")
+        val response = signUpHandler.handle(tokenDto)
+
+        assertEquals(
+            response.status,
+            HttpStatusCode.BadRequest
+        )
+        assertTrue(response.dto is ErrorDto)
+    }
+
+    @Test
+    fun `user already exist`() {
+        val userDto = UserDto(
+            id = "id",
+            name = "username",
+            email = "email@email.com"
+        )
+        every {
+            userRepository.findUserByEmail(userDto.email)
+        } returns userDto
+
+        val tokenDto = TokenDto("dXNlcm5hbWU6Y0dGemMzZHZjbVE9OmVtYWlsQGVtYWlsLmNvbQ==")
+        val response = signUpHandler.handle(tokenDto)
+
+        assertEquals(
+            response.status,
+            HttpStatusCode.BadRequest
+        )
+        assertTrue(response.dto is ErrorDto)
+    }
+
+    @Test
+    fun `successful user creation`() {
+        val userDto = UserDto(
+            id = "id",
+            name = "username",
+            email = "email@email.com"
+        )
+        every {
+            userRepository.findUserByEmail(userDto.email)
+        } returns null
+
+        every {
+            userRepository.createUser(userDto.name, any(), userDto.email)
+        } returns userDto
+
+        val tokenDto = TokenDto("dXNlcm5hbWU6Y0dGemMzZHZjbVE9OmVtYWlsQGVtYWlsLmNvbQ==")
+        val response = signUpHandler.handle(tokenDto)
+
+        assertEquals(
+            response.status,
+            HttpStatusCode.Created
+        )
+    }
+}
