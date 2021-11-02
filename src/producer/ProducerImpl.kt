70cc785527f587dd3ad69ae65df4e49c12c62cf2
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2021-11-02 17:27:33 +0100

    add cloud for kafka

--- src/producer/ProducerImpl.kt
+++ src/producer/ProducerImpl.kt
@@ -7,19 +7,25 @@ import org.apache.kafka.common.serialization.StringSerializer
 import java.util.*
 
 class ProducerImpl : Producer {
-
     private val producer = getProducer()
 
     override fun send(value: String) {
-        producer.use { producer ->
-            val record = ProducerRecord("test", value, value)
-            producer.send(record)
-        }
+        val record = ProducerRecord("test", value, value)
+        producer.send(record)
     }
 
     private fun getProducer(): KafkaProducer<String, String> {
         val properties = Properties().apply {
-            setProperty("bootstrap.servers", "localhost:9092")
+            setProperty("bootstrap.servers", System.getenv("KAFKA_DATABASE_INSTANCE"))
+            setProperty("security.protocol", "SASL_SSL")
+            setProperty(
+                "sasl.jaas.config",
+                "org.apache.kafka.common.security.plain.PlainLoginModule " +
+                        "required username='${System.getenv("KAFKA_DATABASE_USER")}' " +
+                        "password='${System.getenv("KAFKA_DATABASE_PASSWORD")}';"
+            )
+            setProperty("sasl.mechanism", "PLAIN")
+            setProperty("client.dns.lookup", "use_all_dns_ips")
             setProperty(ProducerConfig.ACKS_CONFIG, "all")
             setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.qualifiedName)
             setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.qualifiedName)
