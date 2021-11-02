376cd7062aa62b5cdb702c48856aad0ee6257006
Dmitrii Permiakov <38990835+dvpermyakov@users.noreply.github.com>
2021-11-02 17:29:09 +0100

    Kafka cloud (#45)
    * add producer
    
    * add cloud for kafka

--- src/repositories/PollBrokerRepositoryImpl.kt
+++ src/repositories/PollBrokerRepositoryImpl.kt
@@ -1,21 +1,9 @@
 package com.public.poll.repositories
 
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerConfig.*
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.StringSerializer
-import java.util.*
+import com.public.poll.producer.Producer
 
-class PollBrokerRepositoryImpl : PollBrokerRepository {
+class PollBrokerRepositoryImpl(private val producer: Producer) : PollBrokerRepository {
     override fun addPoll(id: String, question: String) {
-        val properties = Properties()
-        properties["bootstrap.servers"] = "localhost:9092"
-        properties[ACKS_CONFIG] = "all"
-        properties[KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.qualifiedName
-        properties[VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.qualifiedName
-        KafkaProducer<String, String>(properties).use { producer ->
-            val record = ProducerRecord("test", id, id)
-            producer.send(record)
-        }
+        producer.send(id)
     }
 }
