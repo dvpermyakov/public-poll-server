51d507db2c411027aa3562c9081a0f78811dfe78
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2021-11-05 23:13:42 +0100

    change

--- src/cache/CacheImpl.kt
+++ src/cache/CacheImpl.kt
@@ -9,15 +9,17 @@ import redis.clients.jedis.HostAndPort
 import redis.clients.jedis.Jedis
 
 class CacheImpl(private val logger: Logger) : Cache {
-    private val jedis = Jedis(HostAndPort(System.getenv("REDIS_IP_ADDRESS"), 6379))
 
     override fun putPoll(key: String, value: PollDto) {
+        val jedis = Jedis(HostAndPort(System.getenv("REDIS_IP_ADDRESS"), 6379))
         logger.info("ip address = ${System.getenv("REDIS_IP_ADDRESS")}")
         val json = Json.encodeToString(value)
         jedis.set(key, json)
     }
 
     override fun getPoll(key: String): PollDto? {
+        val jedis = Jedis(HostAndPort(System.getenv("REDIS_IP_ADDRESS"), 6379))
+        logger.info("ip address = ${System.getenv("REDIS_IP_ADDRESS")}")
         val json = jedis.get(key)
         return json?.let { Json.decodeFromString<PollDto>(it) }
     }
