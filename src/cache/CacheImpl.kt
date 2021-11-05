79a122ef8f8d431b698ca97c84472d6273e49cd7
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2021-11-05 22:29:35 +0100

    add redis api address

--- src/cache/CacheImpl.kt
+++ src/cache/CacheImpl.kt
@@ -1,18 +1,22 @@
 package com.public.poll.cache
 
 import com.public.poll.dto.PollDto
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import redis.clients.jedis.HostAndPort
+import redis.clients.jedis.Jedis
 
 class CacheImpl : Cache {
-//    private val jedis = Jedis(HostAndPort("127.0.0.1", 6379))
+    private val jedis = Jedis(HostAndPort(System.getenv("REDIS_IP_ADDRESS"), 6379))
 
     override fun putPoll(key: String, value: PollDto) {
-//        val json = Json.encodeToString(value)
-//        jedis.set(key, json)
+        val json = Json.encodeToString(value)
+        jedis.set(key, json)
     }
 
     override fun getPoll(key: String): PollDto? {
-        return null
-//        val json = jedis.get(key)
-//        return json?.let { Json.decodeFromString<PollDto>(it) }
+        val json = jedis.get(key)
+        return json?.let { Json.decodeFromString<PollDto>(it) }
     }
 }
