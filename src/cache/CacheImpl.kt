d13e0ba90882b09a79dd45b8256ee4ac6159f1d0
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2021-11-07 18:16:06 +0100

    remove logger

--- src/cache/CacheImpl.kt
+++ src/cache/CacheImpl.kt
@@ -4,16 +4,11 @@ import com.public.poll.dto.PollDto
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.slf4j.Logger
 import redis.clients.jedis.HostAndPort
 import redis.clients.jedis.Jedis
 
-class CacheImpl(private val logger: Logger) : Cache {
+class CacheImpl : Cache {
     private val jedis = Jedis(HostAndPort(System.getenv("REDIS_IP_ADDRESS"), 6379))
-
-    init {
-        logger.info("redis ip address = ${System.getenv("REDIS_IP_ADDRESS")}")
-    }
 
     override fun putPoll(key: String, value: PollDto) {
         val json = Json.encodeToString(value)
