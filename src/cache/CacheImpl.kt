e6af0c7a94b96f751560e82bca59e831f09b8060
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2021-11-07 17:12:48 +0100

    add redis

--- src/cache/CacheImpl.kt
+++ src/cache/CacheImpl.kt
@@ -1,14 +1,18 @@
 package com.public.poll.cache
 
 import com.public.poll.dto.PollDto
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.slf4j.Logger
+import redis.clients.jedis.HostAndPort
+import redis.clients.jedis.Jedis
 
 class CacheImpl(private val logger: Logger) : Cache {
-//    private val jedis = Jedis(HostAndPort(System.getenv("REDIS_IP_ADDRESS"), 6379))
+    private val jedis = Jedis(HostAndPort(System.getenv("REDIS_IP_ADDRESS"), 6379))
 
     override fun putPoll(key: String, value: PollDto) {
-//        val json = Json.encodeToString(value)
-//        jedis.set(key, json)
+        val json = Json.encodeToString(value)
+        jedis.set(key, json)
     }
 
     override fun getPoll(key: String): PollDto? {
